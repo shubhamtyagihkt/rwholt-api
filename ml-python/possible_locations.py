24bd0e01966e5737bd8f708a27b67e15f150d657
Shubham Tyagi <shubhamtyagihkt@gmail.com>
2019-03-02 19:32:46 +0530

    another silly

--- ml-python/possible_locations.py
+++ ml-python/possible_locations.py
@@ -4,6 +4,6 @@ from random import uniform
 random_points = []
 for i in range(10):
     x, y = uniform(28.6041, 28.9041), uniform(77.0025, 77.20025)
-    random_points.append({'longitude': x, 'latitude': y})
+    random_points.append({'longitude': y, 'latitude': x})
 print(json.dumps(random_points))
 sys.stdout.flush()
