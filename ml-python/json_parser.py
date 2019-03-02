e61dc8617af1b8eb911853e1f8c27a9507f30d7a
Shubham Tyagi <shubhamtyagihkt@gmail.com>
2019-03-02 23:56:03 +0530

    catchment area

--- ml-python/json_parser.py
+++ ml-python/json_parser.py
@@ -1,19 +1,17 @@
 import sys
 import json
-import numpy as np
 
-file = 'delhi_highway.json'
+file = 'C:/Users/Shubham Tyagi/Desktop/delhi_highway.json'
 with open(file, "r") as read_file:
     data = json.load(read_file)
-
 coord = []
 for line in data['features']:
     coord.append(line['geometry']['coordinates'])
-	
 new = []
 for line in coord:
     for points in line:
-        new.append(points)
-new_np = np.unique(np.array(new),axis=0)
-print(json.dumps(new_np).tolist())
+        new.append({'lng': points[0],
+                    'lat': points[1]
+                    })
+print(json.dumps(new))
 sys.stdout.flush()
