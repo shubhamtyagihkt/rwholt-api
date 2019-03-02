20b552686b1cef2d3e617dab6417207884cc7c8b
Shubham Tyagi <shubhamtyagihkt@gmail.com>
2019-03-03 01:21:26 +0530

    error resolve

--- ml-python/json_parser.py
+++ ml-python/json_parser.py
@@ -1,17 +1,24 @@
 import sys
 import json
+import numpy as np
 
-file = 'delhi_highway.json'
+file = 'ml-python/delhi_highway.json'
 with open(file, "r") as read_file:
     data = json.load(read_file)
+
 coord = []
 for line in data['features']:
     coord.append(line['geometry']['coordinates'])
+	
 new = []
 for line in coord:
     for points in line:
-        new.append({'lng': points[0],
-                    'lat': points[1]
-                    })
+        new.append(points)
+new_np = np.unique(np.array(new),axis=0)
+new = []
+for line in new_np:
+    new.append({'lng': points[0],
+                'lat': points[1]
+    }) 
 print(json.dumps(new))
 sys.stdout.flush()
--- routes/index.js
+++ routes/index.js
@@ -14,10 +14,10 @@ router.get('/possible-locations', function(req, res) {
                 req.query.funds, // starting funds
                 req.query.size, // (initial) wager size
                 req.query.count, // wager count — number of wagers per sim
-                req.query.sims // number of simulations
+                req.query.sims // number of simulation
             ]
     };
-    PythonShell.run('ml-python/possible_locations.py', options, function (err, data) {
+    PythonShell.run('ml-python/json_parser.py', options, function (err, data) {
         if (err) res.send(err);
         res.send(data.toString());
     });
