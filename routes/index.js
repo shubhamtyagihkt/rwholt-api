4b97a46b146a5b855a27d71a0dbe652c11fc0fa3
Shubham Tyagi <shubhamtyagihkt@gmail.com>
2019-03-03 09:22:35 +0530

    cost-estimation v1

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,36 @@ router.get('/possible-locations', function(req, res) {
         res.send(data.toString());
     });
 });
+
+router.get('/locations', function(req, res) {
+    var options = {
+        args:
+            [
+                req.query.funds, // starting funds
+                req.query.size, // (initial) wager size
+                req.query.count, // wager count — number of wagers per sim
+                req.query.sims // number of simulation
+            ]
+    };
+    PythonShell.run('ml-python/possible_locations.py', options, function (err, data) {
+        if (err) res.send(err);
+        res.send(data.toString());
+    });
+});
+
+router.get('/cost-estimation', function(req, res) {
+    var options = {
+        args:
+            [
+                req.query.funds, // starting funds
+                req.query.size, // (initial) wager size
+                req.query.count, // wager count — number of wagers per sim
+                req.query.sims // number of simulation
+            ]
+    };
+    PythonShell.run('ml-python/CALCULATING_THE_COST_OF_SETUP.py', options, function (err, data) {
+        if (err) res.send(err);
+        res.send(data);
+    });
+});
 module.exports = router;
